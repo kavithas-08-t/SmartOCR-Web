65c819a893079be317c4ba0a3f28c468c213bb55
kavithas-08-t <kavithast08@gmail.com>
2026-06-21 01:55:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -114,8 +114,15 @@ def download_text():
 
 
 if __name__ == "__main__":
+
+    port = int(
+        os.environ.get(
+            "PORT",
+            5000
+        )
+    )
+
     app.run(
         host="0.0.0.0",
-        port=5000,
-        debug=True
+        port=port
     )
