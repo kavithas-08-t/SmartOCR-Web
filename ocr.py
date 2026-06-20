f4bbeef7709dd684e18f8abe364a540892ea373a
kavithas-08-t <kavithast08@gmail.com>
2026-06-21 01:42:42 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,8 +1,6 @@
 from PIL import Image
 import pytesseract
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
 def extract_text(image_path):
 
     image = Image.open(image_path)
